4f742232e3f756a7699c7d9f3e724bca519d0342
Benji Asperheim <basperheim@gmail.com>
2024-08-14 16:50:44 +0800

    Changed port configuration and Heroku start script

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist", "index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
